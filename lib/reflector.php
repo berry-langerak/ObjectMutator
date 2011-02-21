2b1c35c6d4bc7e386fbb597f4da481eecf6a4876
Berry Langerak <berry@berryllium.nl>
2011-02-21 11:03:39 +0100

    Added more API documentation for Reflector.

--- lib/reflector.php
+++ lib/reflector.php
@@ -1,9 +1,25 @@
 <?php
 namespace ObjectMutator;
 
+/**
+ * An API of the default Reflection API, which caches previously created
+ * reflections and can provide valuable information.
+ *
+ * @author Berry Langerak <berry@berryllium.nl>
+ */
 class Reflector {
+	/**
+	 * Contains an array of instantiated \ReflectionClass objects.
+	 *
+	 * @var \ReflectionClass[]
+	 */
 	protected $reflections = array( );
 
+	/**
+	 * Contains an array of instantiated \ReflectionMethod objects.
+	 *
+	 * @var \ReflectionMethods[]
+	 */
 	protected $methods = array( );
 
 	/**
@@ -21,10 +37,38 @@ class Reflector {
 		return $this->reflections[$classname];
 	}
 
+	/**
+	 * Returns whether or not the method $method exists on the object $object.
+	 *
+	 * @param object $object
+	 * @param string $method
+	 * @return boolean
+	 */
 	public function hasMethod( $object, $method ) {
 		return array_key_exists( $method, $this->methods( $this->reflect( $object ) ) );
 	}
 
+	/**
+	 * Returns the a \ReflectionMethod for method $method of of object $object.
+	 *
+	 * @param object $object
+	 * @param string $method
+	 * @return \ReflectionMethod
+	 */
+	public function method( $object, $method ) {
+		if( $this->hasMethod( $object, $method ) ) {
+			$method = $this->reflect( $object )->getMethod( $method );
+			$method->setAccessible( true );
+			return $method;
+		}
+	}
+
+	/**
+	 * Finds and returns an array of all methods for the object of class $class.
+	 *
+	 * @param \ReflectionClass $class
+	 * @return array
+	 */
 	protected function methods( \ReflectionClass $class ) {
 		if( !isset( $this->methods[$class->name] ) ) {
 			foreach( $class->getMethods( ) as $method ) {
@@ -34,15 +78,13 @@ class Reflector {
 		return $this->methods[$class->name];
 	}
 
-	public function method( $object, $method ) {
-		if( $this->hasMethod( $object, $method ) ) {
-			$method = $this->reflect( $object )->getMethod( $method );
-			$method->setAccessible( true );
-			return $method;
-		}
-	}
-
-	public function classname( $object ) {
+	/**
+	 * Returns the classname of object $object.
+	 *
+	 * @param object $object
+	 * @return string
+	 */
+	protected function classname( $object ) {
 		if( !is_object( $object ) ) {
 			throw new \Exception( 
 				sprintf( 'ObjectMutator needs an instance of an object, but "%s" (%s) was given.', $object, \gettype( $object ) )
